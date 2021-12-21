a67703250062aa189b1b57067c148179dfdad86f
Taras Dzikovskyi <36964328+TarasDzikovskyi@users.noreply.github.com>
2021-12-21 10:31:38 +0200

    Delete GoogleMapsWrapper.js

--- client/src/components/GoogleMapsWrapper.js
+++
@@ -1,6 +0,0 @@
-// import {GoogleApiWrapper} from "google-maps-react";
-// import GoogleMaps from "./GoogleMaps";
-//
-// export default GoogleApiWrapper({
-//     apiKey: ('AIzaSyD4C8hJICPEgHM-rpa0sZ1WD0fMEnrBN0g')
-// })(GoogleMaps())
